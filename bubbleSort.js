d3860e085e8ab2b0ea28e0dc6b256461af62c45d
Charles-04 <charlesmir04@gmail.com>
2022-04-12 11:39:42 +0100

    first commit

---
+++ bubbleSort.js
@@ -0,0 +1,19 @@
+"use strict";
+exports.__esModule = true;
+exports.bubbleSort = void 0;
+function bubbleSort(array) {
+    array = array.slice(); // creates a copy of the array
+    for (var i = 0; i < array.length; i++) {
+        for (var j = 0; j < array.length - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                var swap = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = swap;
+            }
+        }
+    }
+    return array;
+}
+exports.bubbleSort = bubbleSort;
+
+console.log(bubbleSort([1, 3, 4, 2, 4, 5, 2, 5, 53, 6]));
